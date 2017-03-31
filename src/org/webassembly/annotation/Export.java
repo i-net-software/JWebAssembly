ec1fc643c23a529b14abc64256c71fe8d1edeb69
Volker Berlin <volker.berlin@googlemail.com>
2017-03-31 20:39:31 +0200

    Add export annotation to mark methods for export

---
+++ src/org/webassembly/annotation/Export.java
@@ -0,0 +1,13 @@
+package org.webassembly.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface Export {
+
+    String name() default "";
+}
