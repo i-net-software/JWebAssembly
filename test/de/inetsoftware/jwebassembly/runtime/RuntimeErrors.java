b5d4825b56c9a008cba121e80abbf29e3292ebeb
Volker Berlin <volker.berlin@googlemail.com>
2017-04-17 08:23:26 +0200

    reorganize tests

--- test/de/inetsoftware/jwebassembly/runtime/RuntimeErrors.java
+++ test/de/inetsoftware/jwebassembly/runtime/RuntimeErrors.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.inetsoftware.jwebassembly.err;
+package de.inetsoftware.jwebassembly.runtime;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
