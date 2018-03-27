01386051af595df98b32e05a1ef404071c4f85a9
Volker Berlin <volker.berlin@googlemail.com>
2018-03-27 20:05:06 +0200

    Test for iflt

--- test/de/inetsoftware/jwebassembly/runtime/ControlFlowOperators.java
+++ test/de/inetsoftware/jwebassembly/runtime/ControlFlowOperators.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Volker Berlin (i-net software)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.inetsoftware.jwebassembly.runtime;
 
 import java.util.ArrayList;
@@ -26,6 +41,7 @@ public class ControlFlowOperators extends AbstractBaseTest {
             ScriptEngine script = val[0];
             addParam( list, script, "ifeq" );
             addParam( list, script, "ifne" );
+            addParam( list, script, "iflt" );
             addParam( list, script, "forLoop" );
         }
         return list;
@@ -51,6 +67,17 @@ public class ControlFlowOperators extends AbstractBaseTest {
             } else {
                 return 76;
             }
+        }
+
+        @Export
+        static int iflt() {
+            int condition = 3;
+            if( condition >= 0 ) {
+                condition = 13;
+            } else {
+                condition = 76;
+            }
+            return condition;
         }
 
         @Export
