7c9a081e96ca72947aef12bc3f9d80e17a86e304
Volker <Volker@Volker.fritz.box>
2018-06-17 21:15:58 +0200

    First step of refactoring with an intermediate model in the memory of the parsed Java bytecode.

---
+++ src/de/inetsoftware/jwebassembly/module/WasmInstruction.java
@@ -0,0 +1,40 @@
+/*
+   Copyright 2018 Volker Berlin (i-net software)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+package de.inetsoftware.jwebassembly.module;
+
+import java.io.IOException;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Base class of all WasmInstruction.
+ * 
+ * @author Volker Berlin
+ *
+ */
+abstract class WasmInstruction {
+
+    /**
+     * Write this instruction to the WASM module.
+     * 
+     * @param writer
+     *            the target writer
+     * @throws IOException
+     *             if any I/O error occur
+     */
+    abstract void writeTo( @Nonnull ModuleWriter writer ) throws IOException;
+}
---
+++ src/de/inetsoftware/jwebassembly/module/WasmLoadStoreInstruction.java
@@ -0,0 +1,65 @@
+/*
+   Copyright 2018 Volker Berlin (i-net software)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+package de.inetsoftware.jwebassembly.module;
+
+import java.io.IOException;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+/**
+ * WasmInstruction for load and store local variables.
+ * 
+ * @author Volker Berlin
+ *
+ */
+class WasmLoadStoreInstruction extends WasmInstruction {
+
+    private boolean               load;
+
+    private int                   idx;
+
+    private LocaleVariableManager localVariables;
+
+    /**
+     * Create an instance of a load/store instruction
+     * 
+     * @param load
+     *            true: if load
+     * @param idx
+     *            the memory/slot idx of the variable
+     * @param localVariables
+     *            the manager for local variables
+     */
+    WasmLoadStoreInstruction( boolean load, @Nonnegative int idx, LocaleVariableManager localVariables ) {
+        this.load = load;
+        this.idx = idx;
+        this.localVariables = localVariables;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    void writeTo( @Nonnull ModuleWriter writer ) throws IOException {
+        idx = localVariables.get( idx ); // translate slot index to position index
+        if( load ) {
+            writer.writeLoad( idx );
+        } else {
+            writer.writeStore( idx );
+        }
+    }
+}
