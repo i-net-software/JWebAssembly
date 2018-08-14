80a8b5d00d0162dd26d1f72d548e5e1a3d72bb0a
Volker <Volker@Volker.fritz.box>
2018-08-14 15:12:21 +0200

    Test for static variables

---
+++ test/de/inetsoftware/jwebassembly/runtime/StaticValues.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 Volker Berlin (i-net software)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.inetsoftware.jwebassembly.runtime;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.ClassRule;
+import org.junit.runners.Parameterized.Parameters;
+
+import de.inetsoftware.jwebassembly.ScriptEngine;
+import de.inetsoftware.jwebassembly.WasmRule;
+import de.inetsoftware.jwebassembly.api.annotation.Export;
+
+public class StaticValues extends AbstractBaseTest {
+
+    @ClassRule
+    public static WasmRule rule = new WasmRule( TestClass.class );
+
+    public StaticValues( ScriptEngine script, String method, Object[] params ) {
+        super( rule, script, method, params );
+    }
+
+    @Parameters( name = "{0}-{1}" )
+    public static Collection<Object[]> data() {
+        ArrayList<Object[]> list = new ArrayList<>();
+        for( ScriptEngine[] val : ScriptEngine.testParams() ) {
+            ScriptEngine script = val[0];
+            addParam( list, script, "abc" );
+        }
+        return list;
+    }
+
+    static class TestClass {
+
+        static int condition;
+
+        @Export
+        static int abc() {
+            condition = 42;
+            return condition;
+        }
+    }
+}
