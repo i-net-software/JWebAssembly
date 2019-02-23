abb6c8a38ad1c3e735c27332c4913fdbab11c6d6
Volker Berlin <volker.berlin@googlemail.com>
2019-02-23 14:16:05 +0100

    enable-exceptions in Wat2Wasm

--- test/de/inetsoftware/jwebassembly/WasmRule.java
+++ test/de/inetsoftware/jwebassembly/WasmRule.java
@@ -171,7 +171,7 @@ public class WasmRule extends TemporaryFolder {
             File wat2WasmFile = new File( getRoot(), "wat2Wasm.wasm" );
             // the wat2wasm tool
             ProcessBuilder processBuilder =
-                            new ProcessBuilder( cmd, watFile.toString(), "-o", wat2WasmFile.toString(), "--enable-saturating-float-to-int", "--enable-sign-extension", "--enable-multi-value" );
+                            new ProcessBuilder( cmd, watFile.toString(), "-o", wat2WasmFile.toString(), "--enable-saturating-float-to-int", "--enable-sign-extension", "--enable-multi-value", "--enable-exceptions" );
             execute( processBuilder );
 
             // create the node script
