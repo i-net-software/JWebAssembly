5e880bab5bbc1b04b47df7144356462a65e9a1ee
Volker Berlin <volker.berlin@googlemail.com>
2019-10-29 18:56:39 +0100

    first string tests

---
+++ test/de/inetsoftware/jwebassembly/runtime/StringOperations.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2019 Volker Berlin (i-net software)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.inetsoftware.jwebassembly.runtime;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Assume;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+
+import de.inetsoftware.jwebassembly.ScriptEngine;
+import de.inetsoftware.jwebassembly.WasmRule;
+import de.inetsoftware.jwebassembly.api.annotation.Export;
+
+public class StringOperations extends AbstractBaseTest {
+
+    @ClassRule
+    public static WasmRule rule = new WasmRule( TestClass.class );
+
+    public StringOperations( ScriptEngine script, String method, Object[] params ) {
+        super( rule, script, method, params );
+    }
+
+    @Parameters( name = "{0}-{1}" )
+    public static Collection<Object[]> data() {
+        ArrayList<Object[]> list = new ArrayList<>();
+        for( ScriptEngine script : ScriptEngine.testEngines() ) {
+            addParam( list, script, "newFromChars" );
+            addParam( list, script, "newFromBytes" );
+        }
+        rule.setTestParameters( list );
+        return list;
+    }
+
+    @Test
+    @Override
+    public void test() {
+        Assume.assumeFalse( (getScriptEngine().name().startsWith( "SpiderMonkey" ) ) ); // SpiderMonkey shell does not have TextDecoder.
+        super.test();
+    }
+
+    static class TestClass {
+
+        @Export
+        static String newFromChars() {
+            char[] chars = {'ä','ö','ü'};
+            return new String( chars );
+        }
+
+        @Export
+        static String newFromBytes() {
+            byte[] bytes = {(byte)0xC3,(byte)0xA4};
+            return new String( bytes );
+        }
+    }
+}
