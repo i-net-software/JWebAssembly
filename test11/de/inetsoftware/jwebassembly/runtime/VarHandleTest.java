c5fcc2d56118af56e5524b044b05266c26c7eed9
Volker Berlin <volker.berlin@googlemail.com>
2023-05-01 13:24:10 +0200

    Tests for Java 11 and Actions for Java 11

---
+++ test11/de/inetsoftware/jwebassembly/runtime/VarHandleTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2023 Volker Berlin (i-net software)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.inetsoftware.jwebassembly.runtime;
+
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+
+import org.junit.ClassRule;
+import org.junit.runners.Parameterized.Parameters;
+
+import de.inetsoftware.jwebassembly.JWebAssembly;
+import de.inetsoftware.jwebassembly.ScriptEngine;
+import de.inetsoftware.jwebassembly.WasmRule;
+import de.inetsoftware.jwebassembly.api.annotation.Export;
+import de.inetsoftware.jwebassembly.web.DOMString;
+import de.inetsoftware.jwebassembly.web.JSObject;
+
+/**
+ * @author Volker Berlin
+ */
+public class VarHandleTest extends AbstractBaseTest {
+
+    @ClassRule
+    public static WasmRule rule = new WasmRule( TestClass.class );
+
+    public VarHandleTest( ScriptEngine script, String method, Object[] params ) {
+        super( rule, script, method, params );
+    }
+
+    @Parameters( name = "{0}-{1}" )
+    public static Collection<Object[]> data() {
+        ArrayList<Object[]> list = new ArrayList<>();
+        for( ScriptEngine script : ScriptEngine.testEngines() ) {
+            addParam( list, script, "set" );
+        }
+        rule.setTestParameters( list );
+        JWebAssembly.LOGGER.setLevel( Level.FINE );
+        rule.setProperty( JWebAssembly.IGNORE_NATIVE, "true" );
+
+        return list;
+    }
+
+    static class TestClass {
+
+        private static final VarHandle STR;
+        static {
+            try {
+                MethodHandles.Lookup l = MethodHandles.lookup();
+                STR = l.findVarHandle(TestData.class, "str", String.class);
+            } catch (ReflectiveOperationException e) {
+                throw new ExceptionInInitializerError(e);
+            }
+        }
+
+        @Export
+        static DOMString set() {
+            TestData data = new TestData();
+            STR.set( data, "abc" );
+            return JSObject.domString( data.str );
+        }
+    }
+
+    static class TestData {
+        String str;
+    }
+}
